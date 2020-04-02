7dad56f5f4e23e4bded0bd84af0b739d68e14824
Masrur Sakib <mashrur333@gmail.com>
2020-04-02 11:12:53 +0600

    Update 01

--- src/App.js
+++ src/App.js
@@ -5,20 +5,22 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>I couldn’t submit the restaurant (hot onion) assignment in time which was may be within 22 March for <strong>family problem (Land Property Issue)</strong>. Now the next deadline of submitting restaurant assignment is 14 April. Now I’m on module 30, so if I don’t start the next module now it will be difficult for me to submit the next assignment on time. <strong> My priority is now to submit the next assignment (Node+Mongo Assignment) on time</strong>, So now I will start the next module and submit the next assignment on time (which is 5 April) and then I’ll work on the restaurant assignment and will try to submit it within 7 April.</p>
+      <h3>I hope you understand. I will be very grateful if you consider my issue. Thanks.</h3>
+      <p>*** Work in progress, Work will be published on 7 April.</p>
+
+      <h3 className='assignment'>My Previous Assignments Links:</h3>
+      <ul>
+        <li><a href="https://masrursakib-first-project.web.app/" target="_blank">Ema John Assignment</a></li>
+        <li><a href="https://masrursakib-react-third-project.netlify.com/" target="_blank">Firebase Create Account + Sign In System </a></li>
+        <li><a href="https://masrursakib-react-second-project.netlify.com/" target="_blank">Social Platform for Corporate People </a></li>
+        <li><a href="https://sakib333.github.io/third_project_shopping_cart/" target="_blank">Shopping Cart </a></li>
+        <li><a href="https://sakib333.github.io/bank_deposit_withdraw_calculator/" target="_blank">Bank Deposit and Withdraw </a></li>
+        <li><a href="https://sakib333.github.io/second_project/" target="_blank">Ecommerce Website (Html+Bootstrap+JS) </a></li>
+        <li><a href="https://sakib333.github.io/panda_commerce/" target="_blank">Panda Commerce (Html+Bootstrap) </a></li>
+        <li><a href="https://sakib333.github.io/first_project/" target="_blank">Web Portfolio </a></li>
+      </ul>
+
     </div>
   );
 }
